6529f01113439737f7dd553cd62b99f8af1eb423
Code_with_Jegan <jeganfse@gmail.com>
2022-05-30 13:00:19 +0530

    Bootstrap - Installation, Configuration and demo

---
+++ src/app/bootstrap-demo/bootstrap-demo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bootstrap-demo',
+  templateUrl: './bootstrap-demo.component.html',
+  styleUrls: ['./bootstrap-demo.component.css']
+})
+export class BootstrapDemoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
